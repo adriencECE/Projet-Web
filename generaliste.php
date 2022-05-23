f039b360eaf1dd8a87a509037201ee0c7f67c24b
adriencECE <adrien.cailleau@edu.ece.fr>
2022-05-23 14:59:16 +0200

    Afficher liste generalistes

--- generaliste.php
+++ generaliste.php
@@ -7,6 +7,8 @@ $site = "localhost"; //Name of the Website
 $db_id = "root"; //DB login ID
 $db_mdp = ""; //DB login PW
 $var;
+$sql = "";
+$test="";
 //Connect
 //$db_handle = mysqli_connect($site, $db_id, $db_mdp, $db, $port);
 $db_handle = mysqli_connect($site, $db_id, $db_mdp);
@@ -23,20 +25,70 @@ if($db_found){
         $sql = "SELECT * FROM medecins WHERE Spe='Generaliste'";
         $res = mysqli_query($db_handle, $sql);
         //var_dump($res);
-      
+        $listeGeneralistes;
         while($data = mysqli_fetch_assoc($res))
         {
-            echo "Nom: ".$data["Nom"]."  ";
-            echo "Prenom :".$data["Prenom"]."  ";
-            echo "Specialite :".$data["Spe"]."  ";
-            echo "Telephone :".$data["Tel"]."  ";
-            echo "Mail :".$data["Mail"]."  ";
-           
+            //$data = une ligne de la table
+            //On crée un tableau avec toutes ces lignes
+            $listeGeneralistes[]=$data;
         }
-    }
+   
+        
+}
 else{
     echo "Unable to connect <br>";
 }
-
-
 ?>
+
+<html>
+
+<head>
+    <title>OMNES Sant&eacute;-G&eacute;n&eacute;ralistes</title>
+    <link href="OMNESSante.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" type="image/x-icon"
+        href="https://www.omneseducation.com/app/themes/inseec-group/favicon.ico">
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <div id="logo">
+                <img src="https://master-7rqtwti-gxvbzt6usscty.fr-4.platformsh.site//app/uploads/2021/09/Logo-blanc-violet.png"
+                    class="custom-logo">
+            </div>
+        </div>
+        <div id="nav">
+            <a href="Accueil.html">
+                <input type="button" name="Accueil" value="Accueil">
+            </a>
+            <a href="ToutParcourir.html">
+                <input type="button" name="Parcourir" value="Tout Parcourir">
+            </a>
+            <a href="Accueil.html">
+                <input type="button" name="Modifier" value="Modifier">
+            </a>
+            <a href="Compte.html">
+                <input type="button" name="Compte" value="Compte">
+            </a>
+        </div>
+        <div id="section">
+            Liste M&eacute;decins G&eacute;n&eacute;ralistes <br>
+            <ul>
+                <!-- Pour chaque médecin Généraliste dans la table-->
+                <?php foreach($listeGeneralistes as $Generaliste) :?>
+                    <li>
+                        <a href="InfosMedecin.html">
+                        <?php echo $Generaliste['Nom']." "?> 
+                        </a>
+                        <?php echo $Generaliste['Prenom']." ".$Generaliste['Spe']." ".$Generaliste['Tel']." ".$Generaliste['Mail']?>
+                    </li>
+                <?php endforeach?>
+        </ul>
+        </div>
+        <div id="footer">Copyright &copy; 2022, OMNES Sant&eacute;<br>
+            <a href="mailto:OMNES.sante@gmail.com">OMNES.sante@gmail.com</a>
+        </div>
+    </div>
+</body>
+
+</html>
