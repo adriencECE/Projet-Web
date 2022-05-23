9ae9e2f32674e21fdd0331fd46756c0745a6d1cf
adriencECE <adrien.cailleau@edu.ece.fr>
2022-05-23 18:18:27 +0200

    Liste specialistes

--- specialiste.php
+++ specialiste.php
@@ -8,7 +8,13 @@ $db_id = "root"; //DB login ID
 $db_mdp = ""; //DB login PW
 $var;
 $sql = "";
-$test="";
+if(isset($_POST["spe"])){
+    $spe=$_POST["spe"];
+}
+else{
+    $spe = "";
+}
+    
 //Connect
 //$db_handle = mysqli_connect($site, $db_id, $db_mdp, $db, $port);
 $db_handle = mysqli_connect($site, $db_id, $db_mdp);
@@ -22,7 +28,13 @@ $db_found = mysqli_select_db($db_handle,$db);
 
 if($db_found){
     //echo "Connected to DB <br>";
+    if($spe!=""){
+        $sql = "SELECT * FROM medecins WHERE Spe = '$spe'";
+    }
+    else{
         $sql = "SELECT * FROM medecins WHERE Spe != 'Generaliste'";
+    }
+        
         $res = mysqli_query($db_handle, $sql);
         //var_dump($res);
         $listeSpecialistes;
@@ -32,8 +44,6 @@ if($db_found){
             //On crée un tableau avec toutes ces lignes
             $listeSpecialistes[]=$data;
         }
-   
-        
 }
 else{
     echo "Unable to connect <br>";
@@ -72,8 +82,31 @@ else{
             </a>
         </div>
         <div id="section">
-            Liste M&eacute;decins G&eacute;n&eacute;ralistes <br>
+            <form method="post" action="specialiste.php">
+            Choisissez une Sp&eacute;cialit&eacute;:<br>
+            <input type="radio" name="spe" value="Addictologie" onclick="<?php $spe="Addictologie"?>">
+            <label>Addictologie</label>
+            <input type="radio" name="spe" value="Andrologie" onclick="<?php $spe="Andrologie"?>">
+            <label>Andrologie</label>
+            <input type="radio" name="spe" value="Cardiologie" onclick="<?php $spe="Cardiologie"?>">
+            <label>Cardiologie</label>
+            <input type="radio" name="spe" value="Dermatologie" onclick="<?php $spe="Dermatologie"?>">
+            <label> Dermatologie</label>
+            <input type="radio" name="spe" value="Gastro-H&eacute;pato-Ent&eacute;rologie " onclick="<?php $spe="GHE"?>">
+            <label> Gastro-H&eacute;pato-Ent&eacute;rologie</label>
+            <input type="radio" name="spe" value="Gynecologie" onclick="<?php $spe="Gynecologie"?>">
+            <label> Gyn&eacute;cologie</label>
+            <input type="radio" name="spe" value="IST" onclick="<?php $spe="IST"?>">
+            <label> IST</label>
+            <input type="radio" name="spe" value="Osth&eacute;opathie" onclick="<?php $spe="Ostheopathie"?>"> 
+            <label> Ostheopathie</label>
+            <input type="radio" name="spe" value="" onclick="<?php $spe=""?>"><br>
+            <label> Tous</label>
+            <input type="submit" value="Valider">
+            </form>
+            <?php if(isset($listeSpecialistes)):?>
             <form method="post" action="InfosMedecin.php">
+            Liste M&eacute;decins Sp&eacute;cialistes <br>
             <ul>
                 <!-- Pour chaque médecin Généraliste dans la table-->
                 <?php foreach($listeSpecialistes as $Specialiste) :?>
@@ -87,6 +120,10 @@ else{
             <label>Voir les informations du m&eacute;decin s&eacute;lectionn&eacute;:</label>
             <input type="submit" value="Soumettre">
             </form>
+            
+            <?php else :?>
+                    <p>Il n'y a pas de m&eacute;decin de cette sp&eacute;cialite<br></p>
+            <?php endif?>
         </div>
         <div id="footer">Copyright &copy; 2022, OMNES Sant&eacute;<br>
             <a href="mailto:OMNES.sante@gmail.com">OMNES.sante@gmail.com</a>
