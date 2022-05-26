5f730bf1de05d6699d14ba0d23d2242a07c8f38a
Cyriac <78726949+mencyr@users.noreply.github.com>
2022-05-26 23:58:44 +0200

    version sympa

--- Accueil.php
+++ Accueil.php
@@ -73,7 +73,7 @@ $jsvars = json_encode($vars, JSON_HEX_TAG | JSON_HEX_AMP);
         <a id="infos"></a>
             <h1><span>Bienvenue sur OMNES Sant&eacute; <?php echo $_SESSION["login"] ?><br></span></h1>
             <div class="info">
-                <div>
+                <div><!--
                     Actualit&eacute;s <br>
                     1. Un nouveau brancard pour la pédiatrie
                     Depuis novembre 2021, un nouveau brancard d’urgence néonatale et pédiatrique a été acquis par le CHRU. Il va permettre de transporter jusqu’à 300 enfants nés prématurés par an. Notre unité est le pôle de référence de la Bretagne pour la prise en charge des grands prématurés et des jeunes enfants.
@@ -81,13 +81,54 @@ $jsvars = json_encode($vars, JSON_HEX_TAG | JSON_HEX_AMP);
 
 
                 </div>
-                <div>Ev&eacute;nements <br></div>
+                <div>Ev&eacute;nements <br></div>-->
             </div>
         </div>
     </section>
 
+<!-- Offer Section -->
+<section class="offers">
+    <a id="offers"></a>
+    <div class="offers container">
+      <div class="offer-top">
+        <h1 class="section-title">Offers</h1>
+        <p>We offer you the possibility of receiving specific assistance for your expeditions. 
+           With our teams, we have the experience and the ability to provide you with high-quality,
+           long-term shipments. Our profession, our know-how are at your service to allow you to evolve in the high mountains
+           between safety and to be able to improve yourself every day.
+        </p>
+      </div>
+      <div class="offer-bottom">
+        <div class="offer-item">
+          <div class="icon"><img src="https://img.icons8.com/color/48/000000/medical-doctor.png"/></div>
+          <h2>Blabla</h2>
+          <p>A two-day adventure to discover the Madriu Valley, which was declared a World Heritage Site by UNESCO. 
+            For your convenience, horses will carry your equipment and we will organize an unforgettable bivouac in some remote cabins.</p>
+        </div>
+        <div class="offer-item">
+          <div class="icon"><img src="https://img.icons8.com/ios/50/000000/hospital-3.png"/></div>
+          <h2>Blabla</h2>
+          <p>The Himalayas stretches for more than 2,400 km, from Nanga Parbat in Pakistan in the west to Namche Barwa in the east. 
+             It has three parallel chains arranged of altitude.</p>
+        </div>
+        <div class="offer-item">
+          <div class="icon"><img src="https://img.icons8.com/ios-glyphs/30/000000/implant.png"/></div>
+          <h2>Alps</h2>
+          <p>Mountain passes connecting valleys or countries often exceed 2,000 meters in altitude. 
+             The Alps form a 1,200 kilometer barrier between the Mediterranean Sea and the Danube. The alps are beautiful and as big as huge.</p>
+        </div>
+        <div class="offer-item">
+          <div class="icon"><img src="https://img.icons8.com/fluency-systems-filled/48/000000/fetus.png"/></div>
+          <h2>The Andes Cordillera</h2>
+          <p>The Andes Cordillera is the longest continental mountain range in the world2, 
+             trending north-south along the western coast of South America. It's so nice.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Offer Section -->
 
-    <section class="footer">
+    <section class="footerAcceuil">
         <a id="footer"></a>
         <div class="footer container">
             <div id="footer">
